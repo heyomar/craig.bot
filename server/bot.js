7356b78a0c92ec063bb474d7ca6aec037ba32e1d
Omar Rasool <webdevromar@gmail.com>
2019-03-11 16:23:27 -0500

    Clean up

--- server/bot.js
+++ server/bot.js
@@ -1,11 +1,15 @@
+const request = require('request')
+const cheerio = require('cheerio')
 const Telegraf = require('telegraf')
 
 const TOKEN = '783970827:AAFH5GXBXaQ9QIQ1cvxJGCoya4i4dCGu93g'
-
 const bot = new Telegraf(TOKEN)
+
 bot.start(msg => {
 	msg.reply('Hello back to you')
-	msg.replyWithPhoto('https://picsum.photos/200/300/')
 })
-bot.hears('hi', msg => msg.reply('Hey there'))
+bot.hears('check', msg => {
+	msg.reply('hello')
+})
+
 bot.launch()
