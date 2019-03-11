0abac7e388fc1052c927321ddefb95047c74eece
Omar Rasool <webdevromar@gmail.com>
2019-03-11 16:30:52 -0500

    More cleaning

--- server/bot.js
+++ server/bot.js
@@ -2,7 +2,6 @@ const request = require('request')
 const cheerio = require('cheerio')
 const Telegraf = require('telegraf')
 
-const TOKEN = '783970827:AAFH5GXBXaQ9QIQ1cvxJGCoya4i4dCGu93g'
 const bot = new Telegraf(TOKEN)
 
 bot.start(msg => {
