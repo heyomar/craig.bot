7df95635d03c779dc3ec26800d568c851a3dbb7b
Omar Rasool <webdevromar@gmail.com>
2019-03-11 00:42:07 -0500

    Going to each page to get the data now

--- server/app.js
+++ server/app.js
@@ -2,26 +2,38 @@ const request = require('request')
 const cheerio = require('cheerio')
 
 const URL =
-	'https://stlouis.craigslist.org/search/cto?sort=pricedsc&auto_bodytype=10&hasPic=1&max_price=4000&min_auto_year=2000&min_price=100'
+	'https://stlouis.craigslist.org/search/cto?hasPic=1&search_distance=15&postal=63109&min_price=500&max_price=4000&min_auto_year=2000&auto_bodytype=10'
 
 request(URL, (err, res, html) => {
 	if (!err && res.statusCode == 200) {
 		const $ = cheerio.load(html)
-		let cars = []
+		let carsPermaLinks = []
 
-		$('.result-title').each((i, el) => {
-			const item = $(el).text()
-			const price = $(el)
-				.next()
-				.children('.result-price')
-				.text()
+		$('.result-info').each((i, el) => {
+			const carPageURL = $(el)
+				.children('.result-title')
+				.attr('href')
+			carsPermaLinks.push(carPageURL)
+			// console.log(carsPermaLinks)
+		})
 
-			const posting = {
-				title: item,
-				price: price
-			}
+		carsPermaLinks.forEach(singleURL => {
+			request(singleURL, (err, res, html) => {
+				if (!err && res.statusCode == 200) {
+					const $ = cheerio.load(html)
+					let carsObject = []
 
-			cars.push(posting)
+					$('.attrgroup span').each(function(i, el) {
+						let carSpecItem = $(el).text()
+						// console.log(carSpecItem, carSpecValue)
+						carsObject.push({
+							[i]: `${carSpecItem}`
+						})
+					})
+
+					console.log(carsObject)
+				}
+			})
 		})
 	}
 })
